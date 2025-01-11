fe04929c46b099b581e314ee5fc6e3f5236dd263
Sam Garson <samtgarson@gmail.com>
2025-01-11 19:07:11 +0000

    Remove unneeded Item data class

--- SuperFriend/SuperFriendApp.swift
+++ SuperFriend/SuperFriendApp.swift
@@ -12,7 +12,6 @@ import SwiftData
 struct SuperFriendApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
@@ -25,7 +24,7 @@ struct SuperFriendApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Text("Foo")
         }
         .modelContainer(sharedModelContainer)
     }
